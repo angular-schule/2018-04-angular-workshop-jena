8e0faad912e826522354b134550e6f9a11519a87
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-04-09 12:17:26 +0200

    Styles einbinden, AppComponent aufräumen

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'br';
+  title = 'Book Rating!';
+
+  constructor() {
+    /*setTimeout(() => {
+      this.title = 'Hallo Jena';
+    }, 2000);*/
+  }
 }
