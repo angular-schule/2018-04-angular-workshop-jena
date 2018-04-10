5d39f34391a98f52666889a993f961deb9fe7944
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-04-10 11:49:14 +0200

    Test: BookComponent mit Flag ratingWasCalled

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { By } from '@angular/platform-browser';
 
 describe('BookComponent', () => {
   let component: BookComponent;
@@ -16,10 +17,24 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = { isbn: '', title: '', description: '', rating: 3 };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should call rateUp() when button is clicked', () => {
+    const rateUpBtn = fixture.debugElement.query(By.css('button.testing-rate-up-btn'));
+
+    let ratingWasCalled = false;
+
+    component.rateUp = () => {
+      ratingWasCalled = true;
+    };
+
+    rateUpBtn.nativeElement.click();
+    expect(ratingWasCalled).toBe(true);
   });
 });
